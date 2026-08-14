8a1bb0a053c7d882b08b16820ad9241a600686f5
Ayeshafirdous5 <ayeshafirdous0509@gmail.com>
2026-08-14 23:57:18 +0530

    Python learning journey

---
+++ Day-04-Lists/day04.py
@@ -0,0 +1,21 @@
+# DAY 04 - LISTS
+# PART 1 - LIST FUNDAMENTALS
+
+# Exercise 1 Create a list
+fruits = ["Apple", "Banana", "Mango", "Orange"]
+print(fruits)
+
+# Exercise 2 Different types
+student_information = ["Ayesha", "20", "Hyderabad","8.67"]
+print(student_information)
+
+#Exercise 3 — Access items
+cities = ["Hyderabad", "Mumbai", "Delhi", "Chennai"]
+print(cities[0])
+print(cities[1])
+print(cities[3])
+
+#Exercise 4 — Change an item
+marks = [85, 90, 78, 92, 88]
+marks[2] = 80
+print(marks)
